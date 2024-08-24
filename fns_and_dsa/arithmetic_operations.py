429280ede3543e4ede6e879bfce041fbac7741a7
bambozone <bambozone@gmail.com>
2024-08-24 15:00:05 +0300

    arthematic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,9 @@
 # def greet(name):
 # """Prints a greeting message."""
 # print(f"Hello, {name}!")
-from arithmetic_operations import perform_operation
 
-def main():
    
-  def perform_operation(num1, num2, operation):
+def perform_operation(num1, num2, operation):
     if operation == "add":
           return num1 + num2
     if operation == "substract":
@@ -14,13 +12,7 @@ def main():
            return num1 * num2
     if operation == "divide":
            return num1 / num2 
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
+    
 
 
 
@@ -28,5 +20,6 @@ def main():
 
 
 
-if __name__ == "__main__":
-    main()
+
+
+    
