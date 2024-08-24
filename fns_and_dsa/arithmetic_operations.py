5e8b208f693e2837009a643fef9b0db0576ea24e
bambozone <bambozone@gmail.com>
2024-08-24 15:09:58 +0300

    arthematic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,13 +4,13 @@
 def perform_operation(num1, num2, operation):
     if operation == "add":
        return num1 + num2
-    if operation == "subtract":
+    elif operation == "subtract":
        return num1 - num2
-    if operation == "multiply":
+    elif operation == "multiply":
        return num1 * num2
-    if operation == "divide":
+    elif operation == "divide":
        if num1 == 0 :
-         print ("cannot divide by zero")
+         return "cannot divide by zero"
        else :
           return num1 / num2 
    
