8305bf9ac135decb15e3e976cd5898e40fbac0d1
bambozone <bambozone@gmail.com>
2024-08-24 15:23:11 +0300

    arthematic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,19 +8,11 @@ def perform_operation(num1, num2, operation):
        return num1 - num2
     elif operation == "multiply":
        return num1 * num2
-    elif operation == "divide" and num2 == 0 :
+    elif  num2 == 0 and operation == "divide"  :
        return "Error: Division by zero is not allowed."
     elif operation == "divide" and num2 != 0 :
        return num1 / num2
 
-
-print("Arithmetic Operations")
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-result = perform_operation(num1, num2, operation)
-print(f"Result: {result}")
 
 
 
