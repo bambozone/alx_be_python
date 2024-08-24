a920264517e78ff3f329de0e1bc81b3a1d7c2ebd
bambozone <bambozone@gmail.com>
2024-08-24 15:03:21 +0300

    arthematic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,16 @@
-# def greet(name):
-# """Prints a greeting message."""
-# print(f"Hello, {name}!")
+
 
    
 def perform_operation(num1, num2, operation):
     if operation == "add":
-          return num1 + num2
-    if operation == "substract":
-           return num1 - num2
+       return num1 + num2
+    if operation == "subtract":
+       return num1 - num2
     if operation == "multiply":
-           return num1 * num2
+       return num1 * num2
     if operation == "divide":
-           return num1 / num2 
+       return num1 / num2 
+   
     
 
 
